8fb3447f6443fe2dcce6b01a3eacab7fcfc2cd3e
Bruno Alves Fay <brunofay1@hotmail.com>
2021-12-02 21:11:04 -0300

    att

--- zoo functions/src/getSpeciesByIds.js
+++
@@ -1,9 +0,0 @@
-const { species } = require('../data/zoo_data');
-
-function getSpeciesByIds(...ids) {
-  // seu código aqui
-  const speciesFiltred = ids.map((id) => species.find((specie) => (specie.id.includes(id))))
-  return speciesFiltred;
-}
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
-module.exports = getSpeciesByIds;
